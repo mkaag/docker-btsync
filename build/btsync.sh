fb5e2f907603ad934c2645a6bfa79042663f3f05
Maurice Kaag <mkaag@bluewin.ch>
2014-12-27 14:58:11 +0100

    Use hostname as DeviceName

--- build/btsync.sh
+++ build/btsync.sh
@@ -4,6 +4,9 @@ if [ "$SECRET" == "false" ]; then
     SECRET=`btsync --generate-secret`
 fi
 
+HOSTNAME=`hostname`
+
+set -i "s/MY_SERVER_1/$HOSTNAME/" /btsync/btsync.conf
 sed -i "s/MY_SECRET_1/$SECRET/" /btsync/btsync.conf
 echo "Starting btsync with secret: $SECRET"
 
