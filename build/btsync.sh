4daeaba859aa81ae656622a6d6ca59f725677297
Maurice Kaag <mkaag@bluewin.ch>
2014-12-27 15:08:34 +0100

    Fix typo in sed command

--- build/btsync.sh
+++ build/btsync.sh
@@ -6,7 +6,7 @@ fi
 
 HOSTNAME=`hostname`
 
-set -i "s/MY_SERVER_1/$HOSTNAME/" /btsync/btsync.conf
+sed -i "s/MY_SERVER_1/$HOSTNAME/" /btsync/btsync.conf
 sed -i "s/MY_SECRET_1/$SECRET/" /btsync/btsync.conf
 echo "Starting btsync with secret: $SECRET"
 
