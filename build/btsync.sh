8643a5fe2470ac4dcb72493472cfa8d9ad8b353d
Maurice Kaag <mkaag@me.com>
2015-04-19 23:33:07 +0200

    Use exec to run btsync

--- build/btsync.sh
+++ build/btsync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -eo pipefail
+
 if [ "$SECRET" == "false" ]; then
-    SECRET=`btsync --generate-secret`
+  SECRET=`btsync --generate-secret`
 fi
 
 HOSTNAME=`hostname`
@@ -10,4 +12,4 @@ sed -i "s/MY_SERVER_1/$HOSTNAME/" /btsync/btsync.conf
 sed -i "s/MY_SECRET_1/$SECRET/" /btsync/btsync.conf
 echo "Starting btsync with secret: $SECRET"
 
-/usr/local/bin/btsync --config /btsync/btsync.conf --nodaemon
+exec /usr/local/bin/btsync --config /btsync/btsync.conf --nodaemon
